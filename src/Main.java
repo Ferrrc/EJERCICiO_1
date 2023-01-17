b0d87a3d278866c23bbd91041a6a337d9c323b2c
Fer <ruizcorofer.06qgmail.com>
2023-01-17 10:22:46 -0500

    Funcion suma con tres parametros

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        suma(10,25,40);
+
+        
+    }
+
+    public static  void suma(int a, int b, int c) {
+        int resultant  = a + b + c;
+        System.out.println(resultant);
+    }
+}
